f90f5d1454e95b58803db88dd00fc379def53766
Neil Muller <drnlmuller@gmail.com>
2023-01-07 22:34:36 +0200

    Replace removed compat function

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     pass
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
 
 pyconzadir = os.path.dirname(__file__)
